822dcfaa18b56af0ab992f4504e3c1ded6cab931
Giuliano Crescimbeni <51923291+GiulianoCrescimbeni@users.noreply.github.com>
2025-03-16 11:43:32 +0100

    Bug fix

--- Challenge[#1]/esp32-challenge#1/src/esp32-challenge[#1].ino
+++ Challenge[#1]/esp32-challenge#1/src/esp32-challenge[#1].ino
@@ -17,6 +17,9 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
 }
 
 void setup() {
+
+  // IDLE //
+  unsigned long idle_start = micros();
   Serial.begin(115200);
 
   pinMode(TRIG_PIN, OUTPUT);
@@ -24,25 +27,25 @@ void setup() {
 
   Serial.println("Starting...");
   
-
+  // SENSOR READ //
   unsigned long sensor_start = micros();
-  // Starting the sampling request
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
   // Read the result
-  int duration = pulseIn(ECHO_PIN, HIGH, 25000);
+  int duration = pulseIn(ECHO_PIN, HIGH, 4000);
   unsigned long sensor_end = micros();
-  // Distance conversion in centimeters
-  duration= duration / 58;
-  
-  // Debug info
-  //Serial.println("Distance in CM: ", duration);
+  // END OF SENSOR READ //
 
+  // IDLE //
+  // Distance conversion in centimeters
+  int distance= duration / 58;
 
   // Create the message to send
-  String snd_msg = (duration < 51) ? "OCCUPIED\0" : "FREE\0";
+  String snd_msg = (distance < 51 && distance != 0) ? "OCCUPIED\0" : "FREE\0";
+
+  // WIFI START //
   unsigned long wifi_start = micros();
   WiFi.mode(WIFI_STA);
   
@@ -54,31 +57,40 @@ void setup() {
   peerInfo.encrypt= 0;
 
   esp_now_add_peer(&peerInfo);
+
+  // TRANSMISSION START //
   unsigned long transmission_start = micros();
   esp_now_send(broadcast_addr, (uint8_t*) snd_msg.c_str(), snd_msg.length() + 1);
   unsigned long transmission_end = micros();
+  // TRANSMISSION END //
+
   WiFi.mode(WIFI_OFF);
   unsigned long wifi_end = micros();
-  unsigned long sensor_idle_end = micros();
-  Serial.print("Wifi duration: ");
-  Serial.print(wifi_end - wifi_start );
-  Serial.print("\n");
-  Serial.print("Sensor duration in idle: ");
-  Serial.print(sensor_idle_end - sensor_end);
-  Serial.print("\n");
-  Serial.print("Sensor duration: ");
-  Serial.print(sensor_end - sensor_start);
-  Serial.print("\n");
-  Serial.print("Transmission duration: ");
-  Serial.print(transmission_end - transmission_start);
-  Serial.print("\n");
+  // WIFI END//
 
   int personal_duty_cycle= 3+5; // Personal Code = 107124(03)
   esp_sleep_enable_timer_wakeup(personal_duty_cycle * uS_TO_S);
 
+  // Debug info
+  Serial.print("Distance in CM: ");
+  Serial.print(distance);
+  Serial.print("\n");
+  Serial.println(snd_msg);
+
+  Serial.print("Sensor duration: ");
+  Serial.print(sensor_end - sensor_start);
+  Serial.print("\n");
+  Serial.print("Wifi duration: ");
+  Serial.print(wifi_end - wifi_start );
+  Serial.print("\n");
+  Serial.print("Transmission duration: ");
+  Serial.print(transmission_end - transmission_start);
+  Serial.print("\n");
+  Serial.print("Idle time: ");
+  Serial.print((transmission_start - sensor_end) + (sensor_start - idle_start));
+  Serial.print("\n");
 
   esp_deep_sleep_start();
-
 }
 
 void loop() {
