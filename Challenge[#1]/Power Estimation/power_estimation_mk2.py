a64373039fde04822b356f94ce8ac9e93dae78ba
Giuliano Crescimbeni <51923291+GiulianoCrescimbeni@users.noreply.github.com>
2025-03-16 11:41:49 +0100

    Bug fix

--- Challenge[#1]/Power Estimation/power_estimation_mk2.py
+++ Challenge[#1]/Power Estimation/power_estimation_mk2.py
@@ -6,50 +6,44 @@ print("SECOND VERSION\n")
 ####------ DEEP SLEEP TIMES
 personal_duty_cycle = 3+5; #personal_code= 107124(03)
 sleep_mode_power_= 59.5 * 20 #value in [mW] extracted from the csv file "deep_sleep"
-print("Time in sleeping mode [s]: ", personal_duty_cycle)
-print("Power cost of sleep mode per sec [mW]", sleep_mode_power_)
-print("\n")
 
-###------BOOT POWER
-boot_up_power= 313 * 20 #value in [mW] extracted from the csv file "deep_sleep"
-print("the power requested to boot is [mW]:", boot_up_power)
-
+###------IDLE POWER
+idle_power= 313 * 20 #value in [mW] extracted from the csv file "deep_sleep"
+idle_period= 190432 / 1000000
 
 ###------WIFI-POWER
-wifi_power_= 776 * 20 - boot_up_power #value in [mW] extracted from the csv file "deep_sleep"
-wifi_period= 188650 /1000000 # value in [s] taken from the code simulation
+wifi_power= 776 * 20 - idle_power #value in [mW] extracted from the csv file "deep_sleep"
+wifi_period= 197867 /1000000 # value in [s] taken from the code simulation
 
 ###-----SENSOR POWER
 sensor_read_power= 466 *20 #value in [mW] extracted from the csv file "sensor_read"
-sensor_read_period = 12000 / 1000000 #value in [s] taken from the code simulation
-sensor_idle_power= 331* 20 #value in [mW] extracted from the csv file "sensor_read"
-sensor_idle_period=  252 / 1000000 # value in [s] taken from the code simulation
-
+sensor_read_period = 4053 / 1000000 #value in [s] taken from the code simulation
 
 ###---- TRANSMISSION POWER
 transmission_power= 1239 * 20 #value in [mW] extracted from the csv file "transmission_power"
-transmission_period=  252 / 1000000 # value in [s] taken from the code simulation
-
-total_working_time=  sensor_read_period + wifi_period
+transmission_period=  64 / 1000000 # value in [s] taken from the code simulation
 
 
 deep_sleep_energy_consumption   = sleep_mode_power_ * personal_duty_cycle
-sensor_total_energy_consumption  = sensor_read_power * sensor_read_period + sensor_idle_period *sensor_idle_power
-transmission_energy_consumption = transmission_power * transmission_period
-boot_up_energy_consumption= boot_up_power * total_working_time
-
+sensor_energy_consumption  = sensor_read_power * sensor_read_period
+transmission_energy_consumption = transmission_power * transmission_period + wifi_power * wifi_period
+idle_energy_consumption = idle_power * idle_period
 
 print("• Deep sleep energy consumption [mJ]: ", deep_sleep_energy_consumption)
-print("• Sensor read energy consumption [mJ]: ", sensor_total_energy_consumption)
+print("Deep sleep time [s]: 8")
+print("• Sensor read energy consumption [mJ]: ", sensor_energy_consumption)
+print("Sensor read time [s]: ", sensor_read_period)
 print("• Transmission energy consumption [mJ]: ", transmission_energy_consumption)
-print("• Boot up and working chip consumption [mJ]:", boot_up_energy_consumption)
+print("Trasnsmission time [s]: ", transmission_period + wifi_period)
+print("• Idle energy consumption [mJ]:", idle_energy_consumption)
+print("Idle time [s]: ", idle_period)
 print("\n")
 
 #FINAL COSTS
-energy_consumption_transition_cycle= (deep_sleep_energy_consumption + sensor_total_energy_consumption +transmission_energy_consumption +boot_up_energy_consumption)/1000
+energy_consumption_transition_cycle= (deep_sleep_energy_consumption + sensor_energy_consumption + transmission_energy_consumption + idle_energy_consumption)/1000
 print("• Total energy consumption per cycle [J]:", energy_consumption_transition_cycle)
 
-total_duty_cycle_sec= personal_duty_cycle + total_working_time
+total_duty_cycle_sec= personal_duty_cycle + idle_period + wifi_period + sensor_read_period + transmission_period
 print("Total duty cycle [s]: ", total_duty_cycle_sec)
 print("\n")
 
